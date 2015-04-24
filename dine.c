1fe8c8d95d11e2d24726ab0df2095bd0f9255fd0
mpthornbrugh <mpthornbrugh@gmail.com>
2015-04-23 19:38:38 -0500

    Things

--- dine.c
+++ dine.c
@@ -261,8 +261,8 @@ int check_for_deadlock()
     }
     else 	{
     		deadlock = 0;
-			user_time[i] += new_user_time;
-			user_progress[i] += user_time[i];
+			user_time[i] = user_time[i] + new_user_time;
+			user_progress[i] = user_time[i] - user_progress[i];
     }
     
       
